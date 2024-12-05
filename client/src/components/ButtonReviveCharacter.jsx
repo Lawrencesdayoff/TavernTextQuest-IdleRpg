a5f69a33afc38d28a357f3f5730e55f642b14c03
lawrencesdayoff <lawrencecarroll07@outlook.com>
2024-12-05 00:48:11 -0800

    modified:   client/src/components/ButtonReviveCharacter.jsx

--- client/src/components/ButtonReviveCharacter.jsx
+++ client/src/components/ButtonReviveCharacter.jsx
@@ -5,6 +5,7 @@ const ButtonReviveCharacter = (props) => {
     const {characterid, onReviveCharacterClick} = props;
     const onReviveCharacter = async () => {
         await axios.patch(`http://localhost:9999/api/reviveCharacter/${characterid}`)
+        onReviveCharacter()
     } 
     return(
         <>
